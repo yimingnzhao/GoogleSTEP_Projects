a6028c65a0e8c34a7db8b8a2b2f4ba901e1ff4a5
Nathan Zhao <90603yahoo@gmail.com>
2020-06-25 01:33:04 +0000

    add typing animations for introduction

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const TYPEWRITER_TEXT = new Map([
+    ['electrical engineers,', 'a computer scientist'],
+    ['computer scientists,', 'an electrical engineer'],
+    ['musicians,', 'a bass clarinetist'],
+    ['my siblings,', 'a nerd'],
+    ['Pokemon trainers,', 'a Snorlax'], 
+    ['philosophers,', 'a virture seeker'],
+]);
+
+
 /**
  * Adds a random greeting to the page.
  */
@@ -27,8 +37,121 @@ function addRandomGreeting() {
   greetingContainer.innerText = greeting;
 }
 
+/**
+ * TxtRotate class defines the typewritter animation
+ */
+class TxtRotate {
 
+    static animateLine1 = true;
+    static animateLine2 = false;
+
+    /**
+     * Constructor of a TxtRotate object, and begins typewriter animation
+     * @param {element} el The HTML element that will show typewriter animation
+     * @param {array} toRotate The strings for the typewriter to rotate through
+     * @param {number} period The period of a tick when displaying full string (in milliseconds)
+     * @param {number} id The unique id of a 'txt-rotate' class object
+     */
+    constructor(el, toRotate, period, id) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.period = period;
+        this.id = id;
+        this.txt = "";
+        this.loopNum = 0;
+        this.tick();
+        this.isDeleting = false;
+    }
+
+    /**
+     * Performs a single insertion or deletion of a character
+     */
+    tick() {
+        // Gets the current string that will be displayed
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        // Updates the current string based on whether characters are being inserted or deleted
+        if (this.id == 1 && TxtRotate.animateLine1 || this.id == 2 && TxtRotate.animateLine2) {
+            if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        }
+
+        var self = this;
+
+        // Gives randomness in speed of each intermediate tick
+        var delta = 150 - Math.random() * 100;
+
+        // Ensures deleting tick speeds are on average twice the speed of insertion ticks
+        if (this.isDeleting) { delta /= 2; }
+
+        // Uses default tick speeds when the string is complete of when the string is empty
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+            if (this.id == 1) {
+                TxtRotate.animateLine1 = false;
+                TxtRotate.animateLine2 = true;
+            }
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+            if (this.id == 2) {
+                TxtRotate.animateLine1 = true;
+                TxtRotate.animateLine2 = false;
+            }
+        }
+
+        // Sets the timeout for each tick
+        setTimeout(function() {
+            self.tick();
+            }, delta);
+    }
+}
+
+/**
+ * Begins typewriter animation for each 'txt-rotate' class tag
+ */
+function startTypewriterAnimation() {
+    // Creates arrays for typewriter animation strings
+    var rotateStrings1 = [];
+    var rotateStrings2 = [];
+    for (const [k, v] of TYPEWRITER_TEXT) {
+        rotateStrings1.push(k);
+        rotateStrings2.push(v);
+    }
+
+    // Gets each element of class 'txt-rotate' and begins a new animation for each one
+    var elements = $('.txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var period = parseInt(elements[i].getAttribute('data-period'), 10) || 200;
+        var id = parseInt(elements[i].getAttribute('data-id'), 10) || 0;
+        if (id == 1) {
+            new TxtRotate(elements[i], rotateStrings1, period, id);
+        } else if (id == 2) {
+            new TxtRotate(elements[i], rotateStrings2, period, id);
+        }
+    }
+
+    // Inject CSS to create animated cursor 
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+}
+
+
+/**
+ * Executes when document is loaded
+ */
  $(document).ready(function() {
+
+     // Animates the picture slideshow
      $("#slideshow > div:gt(0)").hide();
      setInterval(function() {
          $('#slideshow > div:first')
@@ -38,4 +161,19 @@ function addRandomGreeting() {
          .end()
          .appendTo('#slideshow');
      }, 3000);
+
+     window.onload = function() {
+
+     }
+
+
+window.onload = function() {
+    var animateLine1 = true;
+    var animateLine2 = false;
+    startTypewriterAnimation();
+};
+
+
+
+
  });
