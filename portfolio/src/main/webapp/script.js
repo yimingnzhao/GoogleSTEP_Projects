686979903867ca374b04076c3a4d46af327ba372
Nathan Zhao <90603yahoo@gmail.com>
2020-07-07 17:56:59 +0000

    insert hard-coded graph

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -284,6 +284,28 @@ function escapeHtml(text) {
 }
 
 
+function drawChart() {
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Animal');
+    data.addColumn('number', 'Count');
+    data.addRows([
+        ['Lions', 10],
+        ['Tigers', 5],
+        ['Bears', 15]
+    ]);
+
+    const options = {
+        'title': 'Zoo Animals',
+        'width':500,
+        'height':400
+    };
+
+    const chart = new google.visualization.PieChart(
+        document.getElementById('chart-container'));
+    chart.draw(data, options);
+}
+
+
 /**
  * Executes when document is loaded
  */
@@ -369,5 +391,8 @@ function escapeHtml(text) {
     });
 
     manageLogin();
+
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
     
  });
