f7997bd505f1484fc121fa31e9eb1b1d5801ee1c
Nathan Zhao <90603yahoo@gmail.com>
2020-07-09 17:31:04 +0000

    fix potential null request parameters

--- portfolio/src/main/java/com/google/sps/servlets/TranslateServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslateServlet.java
@@ -22,6 +22,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 /** Servelet that translates inputted text */
 @WebServlet("/translate")
@@ -32,6 +33,10 @@ public class TranslateServlet extends HttpServlet {
     private static final String REQUEST_MOCK_PARAM = "mock";
     private static final String RESPONSE_TEXT_CONTENT = "text/html; charset=UTF-8";
     private static final String RESPONSE_CHAR_ENCODING = "UTF-8";
+    private static final String EMPTY_RESPONSE = "";
+    private static final String INVALID_LANGUAGE_CODE_EXCEPTION = "Exception: Invalid language code used";
+    private static final String NULL_REQUEST_PARAMETERS_EXCEPTION = "Exception: Null text or language code parameters";
+    private static String LANGUAGE_CODES_ARRAY[] = {"en", "zh", "es", "hi", "ar"};
 
     /**
      * Gets the translated message based on front end text and language code
@@ -45,6 +50,23 @@ public class TranslateServlet extends HttpServlet {
         String languageCode = request.getParameter(REQUEST_LANGUAGE_CODE_PARAM);
         String mock = request.getParameter(REQUEST_MOCK_PARAM);
 
+        // Set the response type
+        response.setContentType(RESPONSE_TEXT_CONTENT);
+        response.setCharacterEncoding(RESPONSE_CHAR_ENCODING);
+
+        if (originalText == null || languageCode == null) {
+            System.out.println(NULL_REQUEST_PARAMETERS_EXCEPTION);
+            response.getWriter().print(EMPTY_RESPONSE);
+            return;
+        }
+
+        // Checks for validity of request parameters
+        if (Arrays.asList(LANGUAGE_CODES_ARRAY).indexOf(languageCode) < 0) {
+            System.out.println(INVALID_LANGUAGE_CODE_EXCEPTION);
+            response.getWriter().print(EMPTY_RESPONSE);
+            return;
+        }
+
         // Gives option to mock the Translation API for testing
         String translatedText;
         if (mock != null) {
@@ -57,8 +79,6 @@ public class TranslateServlet extends HttpServlet {
         }
 
         // Output the translation
-        response.setContentType(RESPONSE_TEXT_CONTENT);
-        response.setCharacterEncoding(RESPONSE_CHAR_ENCODING);
-        response.getWriter().println(translatedText);
+        response.getWriter().print(translatedText);
     }
 }
