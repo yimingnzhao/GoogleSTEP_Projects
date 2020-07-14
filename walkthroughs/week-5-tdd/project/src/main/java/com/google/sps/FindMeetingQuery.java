c1a39b990dd6b078159f9470ef3df2cb889f98f7
Nathan Zhao <90603yahoo@gmail.com>
2020-07-14 03:25:17 +0000

    add tests and implementation of including optional set

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -25,14 +25,47 @@ public final class FindMeetingQuery {
   /**
    * Queries a collection of available time ranges that could accommodate the meeting request
    * @param {Collection<Event>} events The collection of events that may conflict with the reqest
-   * @param {MeetingRequest} request The request to query abailable time ranges of
-   * @return {Collection<TimeRange>} The time ranges that could accommodate the meeting request
+   * @param {MeetingRequest} request The request to query available time ranges of
+   * @return Collection<TimeRange> The time ranges that could accommodate the meeting request
    */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    Collection<String> attendeesWithOptional = new ArrayList<String>();
+    Collection<String> attendeesWithoutOptional = new ArrayList<String>();
+    
+    // Creates two collections of attendees for only mandatory and with optional
+    for (String attendee : request.getAttendees()) {
+      attendeesWithOptional.add(attendee);
+      attendeesWithoutOptional.add(attendee);
+    }
+    for (String optional : request.getOptionalAttendees()) {
+      attendeesWithOptional.add(optional);
+    }
+
+    // Queries the time ranges for both collections
+    Collection<TimeRange> rangesWithOptional = queryAllAttendees(events, request, attendeesWithOptional);
+    Collection<TimeRange> rangesWithoutOptional = queryAllAttendees(events, request, attendeesWithoutOptional);
+
+    // Use optional time ranges if there are no mandatory attendees
+    if (request.getAttendees().size() == 0) {
+      return rangesWithOptional;
+    }
+
+    // Else, only use the optional time ranges if there exists an optional time range
+    return (rangesWithOptional.size() > 0) ? rangesWithOptional : rangesWithoutOptional;
+  }
+
+  /**
+   * Queries a collection of available time ranges from a collection of attendees
+   * @param {Collection<Event>} events The collection of events that may conflict with the request
+   * @param {MeetingRequest} request The request to query available time ranges of
+   * @param {Collection<String>} attendees The collection of attendees 
+   * @return Collection<TimeRange> The time ranges that could accommodate the meeting request
+   */
+  public Collection<TimeRange> queryAllAttendees(Collection<Event> events, MeetingRequest request, Collection<String> attendees) {
     // Initializes and gets colllections that will be used locally to query
     List<TimeRange> blockedTimes = new ArrayList<TimeRange>();
     Collection<TimeRange> result = new ArrayList<TimeRange>();
-    Collection<String> requestAttendees = request.getAttendees();
+    Collection<String> requestAttendees = attendees;
 
     // Finds the set of events that block time ranges for being a valid meeting time
     for (Event event : events) {
@@ -58,7 +91,7 @@ public final class FindMeetingQuery {
       }
     });
 
-    // Gets the next available time range by saving the end time of the last blocked time
+    // Greedily gets the next available time range by saving the end time of the last blocked time
     int lastEndTime = TimeRange.START_OF_DAY;
     for (int i = 0; i < blockedTimes.size(); i++) {
       int eventStart = blockedTimes.get(i).start();
@@ -74,6 +107,6 @@ public final class FindMeetingQuery {
       result.add(TimeRange.fromStartEnd(lastEndTime, TimeRange.END_OF_DAY, true));
     }
 
-    return result;
+    return result; 
   }
 }
